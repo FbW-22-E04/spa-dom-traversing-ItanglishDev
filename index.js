db4f001ab8feb522940b826853d559152a136c71
chris <chrishethertonweb@gmail.com>
2022-09-19 12:06:23 +0200

    second point done

--- index.js
+++ index.js
@@ -2,3 +2,16 @@ let heading = document.querySelector('h1')
 let header = heading.closest('header')
 
 header.style.border = '5px solid'
+
+let infoSection = document.querySelector('.info')
+let infoPackage = document.querySelector('.info-package')
+let packageTitle = document.querySelector('package-title')
+
+Array.from(infoSection.children).forEach(element => {
+  if (element) {
+    element.style.border = '2px solid'
+  }
+
+
+});
+// console.log('HERE', Array.from(infoSection));
