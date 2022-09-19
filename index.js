8b0b13964717995cc4fac9b21972b2faed762956
chris <chrishethertonweb@gmail.com>
2022-09-19 13:42:46 +0200

    third point done

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ header.style.border = '5px solid'
 
 let infoSection = document.querySelector('.info')
 let infoPackage = document.querySelector('.info-package')
-let packageTitle = document.querySelector('package-title')
-
+let packageTitle = document.querySelector('.info .package-title')
+let label = document.querySelector('.info .info-package')
 Array.from(infoSection.children).forEach(element => {
   if (element) {
     element.style.border = '2px solid'
@@ -15,3 +15,18 @@ Array.from(infoSection.children).forEach(element => {
 
 });
 // console.log('HERE', Array.from(infoSection));
+
+Array.from(infoSection.children).forEach(el => {
+
+  if (el.children[2].matches('.mild')) {
+    el.children[2].style.border = '2px solid yellow'
+  } else if (el.children[2].matches('.intense')) {
+    el.children[2].style.border = '2px solid orange'
+  } else if (!el.children[2].matches('.mild') || !el.children[2].matches('.intense'))
+    el.children[2].style.border = '2px solid red'
+
+})
+
+// if (label)
+console.log('INFO', infoSection.children);
+
